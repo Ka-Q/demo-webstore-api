31fa7929afb71d97c969179754881743fbee38e2
Ka-Q <akulaurila@gmail.com>
2023-06-01 12:38:42 +0300

    moved index.json

--- api/index.js
+++ api/index.js
@@ -6,6 +6,6 @@ const PORT = 5000;
 
 app.listen(PORT, () => {console.log(`App running in port ${PORT}`)});
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({data: "data"});
 })
