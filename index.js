97f420a7b567f544ff4f2f6647fecc6a2f1916ae
Ka-Q <akulaurila@gmail.com>
2023-06-01 13:02:50 +0300

    add error on ./

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = 5000;
 
 app.listen(PORT, () => {console.log(`App running in port ${PORT}`)});
 
+app.get('/', (req, res) => {
+    res.json({data: "error"});
+})
+
 app.get('/api', (req, res) => {
     res.json({data: "data"});
 })
