462c87b13b3af1251ded7357d0db1bedb285ec58
Ka-Q <akulaurila@gmail.com>
2023-06-01 14:24:16 +0300

    added db connection

--- api/product_functions.js
+++ api/product_functions.js
@@ -1,5 +1,22 @@
-const getProduct = (req, res) => {
-    res.json({product_name: 'Test product', price: 2.99});
+require('dotenv').config()
+const mysql = require('mysql2')
+
+const connect = async (res, table) => {
+    const connection = mysql.createConnection(process.env.DATABASE_URL)
+    connection.execute(`select * from ${table}`, (err, results, fields) => {
+        if (!err) {
+            res.json({data: results});
+        } else {
+            res.status(400);
+            res.json({data: "error"});
+        }
+        
+    });
+    connection.end()
+}
+
+const getProduct = async (req, res) => {
+    connect(res, 'product');
 };
 
 module.exports = getProduct;
