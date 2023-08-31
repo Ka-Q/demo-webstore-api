f4ca4131eb9aa207a47c9d185e5faac5d674e44c
Ka-Q <akulaurila@gmail.com>
2023-08-31 13:31:56 +0300

    updated getProductExpanded to be more flexible

--- api/ProductFunctions.js
+++ api/ProductFunctions.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mysql = require('mysql2');
-const { generateGetSQL, generatePostSQL, generatePutSQL, generateDeleteSQL } = require('./SQLGenerators');
+const { generateGetSQL, generatePostSQL, generatePutSQL, generateDeleteSQL, generateGetSQLFromQuery } = require('./SQLGenerators');
 
 const connect = (res, queryJSON) => {
     const connection = mysql.createConnection(process.env.DATABASE_URL)
@@ -145,14 +145,6 @@ const deleteProductImage = (req, res) => {
 };
 
 const getProductExpanded = (req, res) => {
-    let params = req.query;
-    let productID = params.product_id;
-
-    if (!productID) { 
-        res.json({error: "Missing product ID"});
-        return;
-    }
-
     let query = "SELECT *, image.image_id as product_image_id, product.product_id as product_product_id from product" +
     " LEFT JOIN product_images" +
     " ON product.product_id = product_images.product_id" +
@@ -169,20 +161,15 @@ const getProductExpanded = (req, res) => {
     " LEFT JOIN event" +
     " ON product.event_id = event.event_id" +
     " LEFT JOIN review" +
-    " ON product.product_id = review.product_id" +
-    " WHERE product.product_id = ?";
+    " ON product.product_id = review.product_id";
 
-    console.log(query);
-
-    let queryList = [productID];
+    let queryJSON = generateGetSQLFromQuery(query, req);
 
     const connection = mysql.createConnection(process.env.DATABASE_URL)
-    connection.query(query, queryList, (err, results, fields) => {
+    connection.query(queryJSON.query, queryJSON.queryList, (err, results, fields) => {
         if (!err) {
-            console.log(results);
             res.json({data: cleanResults(results)});
         } else {
-            console.log(err);
             res.status(400);
             res.json({data: "error"});
         }
@@ -192,63 +179,77 @@ const getProductExpanded = (req, res) => {
 }
 
 const cleanResults = (results) => {
-    let product = results[0];
-    if (!product) return {data: []};
-    let cleaned = {}
-    
-    cleaned.product_id = product.product_product_id;
-    cleaned.product_name = product.product_name;
-    cleaned.product_description = product.product_description;
-    cleaned.price = product.price;
-    cleaned.discount = product.discount;
-    if (product.event_id) {
-        cleaned.event = {
-            event_id: product.event_id,
-            event_name: product.event_name,
-            event_description: product.event_description,
-            event_start_date: product.event_start_date,
-            event_end_date: product.event_end_date
+
+    let previousID = null;
+    let cleaned = null;
+    let cleanedResults = [];
+
+    for (let i in results) {
+        let product = results[i];
+        let id = product.product_product_id;
+
+        if (id != previousID) {
+
+            if (cleaned) {
+                cleanedResults.push(cleaned);
+            }
+            previousID = id;
+
+            cleaned = {};
+            cleaned.product_id = product.product_product_id;
+            cleaned.product_name = product.product_name;
+            cleaned.product_description = product.product_description;
+            cleaned.price = product.price;
+            cleaned.discount = product.discount;
+            if (product.event_id) {
+                cleaned.event = {
+                    event_id: product.event_id,
+                    event_name: product.event_name,
+                    event_description: product.event_description,
+                    event_start_date: product.event_start_date,
+                    event_end_date: product.event_end_date
+                }
+            } else {
+                cleaned.event = null;
+            }
+            cleaned.categories = [];
+            cleaned.manufacturers = [];
+            cleaned.images = [];
+            cleaned.reviews = [];
         }
-    } else {
-        cleaned.event = null;
+        else {
+            let row = product;
+            if (row.category_id && !cleaned.categories.find(c => c.category_id == row.category_id)) cleaned.categories.push({
+                category_id: row.category_id,
+                category_name: row.category_name,
+                category_description: row.category_description
+            });
+
+            if (row.manufacturer_id && !cleaned.manufacturers.find(m => m.manufacturer_id == row.manufacturer_id)) cleaned.manufacturers.push({
+                manufacturer_id: row.manufacturer_id,
+                manufacturer_name: row.manufacturer_name
+            });
+
+            if (row.product_image_id && !cleaned.images.find(i => i.image_id == row.product_image_id)) cleaned.images.push({
+                image_id: row.product_image_id,
+                image_source: row.image_source,
+                image_type_id: row.image_type_id,
+                image_description: row.image_description,
+                image_link: row.image_link
+            });
+
+            if (row.review_id && !cleaned.reviews.find(r => r.review_id == row.review_id)) cleaned.reviews.push({
+                review_id: row.review_id,
+                user_id: row.user_id,
+                review_rating: row.review_rating,
+                review_description: row.review_description,
+                review_helpful: row.review_helpful,
+                review_not_helpful: row.review_not_helpful
+            });
+        }
     }
-    cleaned.categories = [];
-    cleaned.manufacturers = [];
-    cleaned.images = [];
-    cleaned.reviews = [];
-
-    for (let row of results) {
-
-        if (row.category_id && !cleaned.categories.find(c => c.category_id == row.category_id)) cleaned.categories.push({
-            category_id: row.category_id,
-            category_name: row.category_name,
-            category_description: row.category_description
-        });
-
-        if (row.manufacturer_id && !cleaned.manufacturers.find(m => m.manufacturer_id == row.manufacturer_id)) cleaned.manufacturers.push({
-            manufacturer_id: row.manufacturer_id,
-            manufacturer_name: row.manufacturer_name
-        });
-
-        if (row.product_image_id && !cleaned.images.find(i => i.image_id == row.product_image_id)) cleaned.images.push({
-            image_id: row.product_image_id,
-            image_source: row.image_source,
-            image_type_id: row.image_type_id,
-            image_description: row.image_description,
-            image_link: row.image_link
-        });
-
-        if (row.review_id && !cleaned.reviews.find(r => r.review_id == row.review_id)) cleaned.reviews.push({
-            review_id: row.review_id,
-            user_id: row.user_id,
-            review_rating: row.review_rating,
-            review_description: row.review_description,
-            review_helpful: row.review_helpful,
-            review_not_helpful: row.review_not_helpful
-        });
-    }
-
-    return cleaned;
+    cleanedResults.push(cleaned);
+    return cleanedResults;
 }
 
 module.exports = {
