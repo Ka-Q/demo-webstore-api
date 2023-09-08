906dc87f4149c3b336ee16a018cd9f8cea851f73
Ka-Q <akulaurila@gmail.com>
2023-09-08 17:25:45 +0300

    Trying S3Client

--- api/ImagefileFunctions.js
+++ api/ImagefileFunctions.js
@@ -1,47 +1,58 @@
 require('dotenv').config()
-const { AwsClient } = require('aws4fetch')
+const { S3Client, GetObjectCommand, PutObjectCommand  } = require("@aws-sdk/client-s3");
 
-const aws = new AwsClient({
-  accessKeyId: process.env.R2_ACCESSKEY,
-  secretAccessKey: process.env.R2_ACCESSKEY_SECRET
-})
+const client = new S3Client({
+    endpoint: process.env.R2_ENDPOINT ,
+    credentials: {
+        accessKeyId: process.env.R2_ACCESSKEY,
+        secretAccessKey: process.env.R2_ACCESSKEY_SECRET,
+        },
+    region: 'auto'
+});
 
-async function getImageFile(req, res) {
-  const filename = req.query.filename;
-  if (!filename) {
-    res.json({error: "missing filename"})
-  }
-  else {
-    try {
-      const response = await aws.fetch(`${process.env.R2_BUCKET_API}/${filename}`)
-      if (!response.ok) throw new Error('No such file')
-      const fileStream = await response.arrayBuffer();
-      res.setHeader('Content-Type', 'image/jpeg');
-      res.send(fileStream);
-    } catch (err) {
-      res.json({error: "No such file"})
+const getImageFile = async (req, res) => {
+    let filename = req.query.filename;
+
+    if (!filename) {
+        res.json({error: "missing filename"})
     }
-  }
+    else {
+        try {
+            const command = new GetObjectCommand({
+                Bucket: process.env.R2_BUCKET,
+                Key: filename
+            });
+    
+            const data = await client.send(command);
+            const fileStream = data.Body;
+            res.setHeader('Content-Type', 'image/jpeg');
+            fileStream.pipe(res);
+        } catch (err) {
+            res.json({error: "no such file"})
+        }
+    } 
 }
 
-async function postImageFile(req, res) {
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.json({error: 'No files were uploaded.'});
-  }
-  let file = req.files.file;
-  let filename = file.name;
-  
-  try {
-    const response = await aws.fetch(`${process.env.R2_BUCKET_API}/${filename}`, {
-      method: 'PUT',
-      body: file.data,
-      headers: { 'Content-Type': 'application/octet-stream' }
-    })
-    if (!response.ok) throw new Error('Error uploading file')
-    res.json({success: "File uploaded successfully"})
-  } catch (err) {
-    res.json({error: "Error uploading file"})
-  }
+const postImageFile = async (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let file = req.files.file;
+    let filename = file.name;
+
+    try {
+        const command = new PutObjectCommand({
+            Bucket: process.env.R2_BUCKET,
+            Key: filename,
+            Body: file.data
+        });
+
+        await client.send(command);
+        res.json({success: "File uploaded successfully"})
+    } catch (err) {
+        res.json({error: "Error uploading file"})
+    }
 }
 
 module.exports = {getImageFile, postImageFile};
