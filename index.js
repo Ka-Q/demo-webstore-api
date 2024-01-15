e4170f108a3a072ebd915342e1303eedf11a20bd
Ka-Q <akulaurila@gmail.com>
2024-01-15 15:47:49 +0200

    added a subdomain to allowed

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const PORT = 5000;
 // CORS app.use(cors());
 
 const allowed = {
-    origin: ["http://localhost:3000","https://dws-front.netlify.app"],
+    origin: ["http://localhost:3000","https://dws-front.netlify.app", "dws.akulaurila.com"],
     default: "http://localhost:3000"
   }
 
