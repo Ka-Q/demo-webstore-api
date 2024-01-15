fdae49f67d431c9473068c66683870eef021033f
Ka-Q <akulaurila@gmail.com>
2024-01-15 14:44:11 +0200

    More CORS

--- index.js
+++ index.js
@@ -29,8 +29,7 @@ const PORT = 5000;
 
 // CORS app.use(cors());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
-    res.header("Access-Control-Allow-Origin", process.env.DOMAIN_URL);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
